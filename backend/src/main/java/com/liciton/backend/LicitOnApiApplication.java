0f1f11a6c1f829c6a6216c3215aa70623cf30c31
Douglas <polak.douglas@gmail.com>
2026-02-18 10:28:39 -0300

    feat: estrutura inicial do projeto

---
+++ backend/src/main/java/com/liciton/backend/LicitOnApiApplication.java
@@ -0,0 +1,13 @@
+package com.liciton.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LicitOnApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LicitOnApiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/liciton/backend/LicitOnApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liciton.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LicitOnApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
